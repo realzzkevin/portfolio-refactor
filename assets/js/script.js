777840072d046ee2b48011d4fba73f8318788102
zzkevin <realzzkevin@gmail.com>
2021-06-28 02:06:20 -0400

    toggle nav-tab function

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+var tabEl = document.querySelectorAll('a[data-toggle="tab"]');
+
+tabEl.forEach(tab => {
+    tab.addEventListener('click', function (event) {
+        var targ = event.target;
+        tabEl.forEach(el =>{
+            el.classList.remove("active");
+        });
+        targ.classList.add("active");
+    })
+    
+});
